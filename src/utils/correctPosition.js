29fdbe27eeeb2a652ad3a4d634adda28bf83183e
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-10-12 08:28:05 +0900

    Refactor: Refactor correctPosition util function

--- src/utils/correctPosition.js
+++ src/utils/correctPosition.js
@@ -1,48 +1,26 @@
 const correctPosition = (position, direction) => {
   const correctedPosition = { ...position };
-  const south = !direction;
-
-  if (south) {
-    return correctedPosition;
-  }
-
-  const { x, z } = correctedPosition;
   const north = (direction === 0.5) || (direction === -0.5);
-
-  if (north) {
-    correctedPosition.x = -x;
-    correctedPosition.z = -z;
-  }
-
   const east = (direction === 0.25) || (direction === -0.75);
-
-  if (east) {
-    if ((x > 0 && z > 0) || (x < 0 && z < 0)) {
-      correctedPosition.x = -z;
-      correctedPosition.z = x;
-    }
-
-    if ((x > 0 && z < 0) || (x < 0 && z > 0)) {
-      correctedPosition.x = -z;
-      correctedPosition.z = x;
-    }
-  }
-
   const west = (direction === 0.75) || (direction === -0.25);
+  const { x, z } = correctedPosition;
 
-  if (west) {
-    if ((x > 0 && z > 0) || (x < 0 && z < 0)) {
+  switch (true) {
+    case east:
+      correctedPosition.x = -z;
+      correctedPosition.z = x;
+      return correctedPosition;
+    case west:
       correctedPosition.x = z;
       correctedPosition.z = -x;
-    }
-
-    if ((x > 0 && z < 0) || (x < 0 && z > 0)) {
-      correctedPosition.x = z;
-      correctedPosition.z = -x;
-    }
+      return correctedPosition;
+    case north:
+      correctedPosition.x = -x;
+      correctedPosition.z = -z;
+      return correctedPosition;
+    default:
+      return correctedPosition;
   }
-
-  return correctedPosition;
 };
 
 export default correctPosition;
