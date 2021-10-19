d08ec599b6f7f4a58bc3817101d54d35c480bba7
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-10-19 19:04:14 +0900

    Remove: Remove function searching ranking by nickname

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,17 +1,12 @@
-import { useRef } from "react";
-
 import useStore from "../../Store/useStore";
 import Text from "../Text/Text";
 import RankingTable from "../RankingTable/RankingTable";
-import NicknameInput from "../NicknameInput/NicknameInput";
 import { RIGHT_ANGLE } from "../../constants/angles";
 import { TITLE_RANKING } from "../../constants/titles";
 import { MAIN } from "../../constants/cameraPositions";
 
 export default function Ranking() {
   const setCameraPosition = useStore((state) => state.setCameraPosition);
-  const inputValue = useRef();
-  const SEARCH = "Search";
   const BACK = "BACK";
 
   const moveMain = () => {
@@ -39,21 +34,6 @@ export default function Ranking() {
         position={[0, 45, 0]}
         rotation={[0, RIGHT_ANGLE, 0]}
         size={8}
-        height={3}
-        bevelEnabled={true}
-        bevelThickness={2}
-        bevelSize={0.4}
-        bevelSegments={10}
-      />
-      <NicknameInput
-        ref={inputValue}
-        position={[0, 30, 15]}
-      />
-      <Text
-        content={SEARCH}
-        position={[0, 30, -25]}
-        rotation={[0, RIGHT_ANGLE, 0]}
-        size={4.5}
         height={3}
         bevelEnabled={true}
         bevelThickness={2}
